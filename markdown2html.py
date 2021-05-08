53dcae5931321b0e129197c9f317e4be1875ef46
maleksal <malek.salem.14@gmail.com>
2021-05-08 02:48:34 +0100

    update & clean code

--- markdown2html.py
+++ markdown2html.py
@@ -17,53 +17,57 @@ class MarkDown:
         self.ord_list = []
         self.unrd_list = []
 
-    def heading(self):
+    def clear_cache(self):
+        """Clear cache.
+        """
+        self.ord_list.clear()
+        self.unrd_list.clear()
+
+    def heading(self, symbol):
+        """Handle html heading parsing.
+        """
         text = self.filecontent[self.position]
-        hdings = text.count("#")
-        markd = text.split('#' * hdings + ' ')
-        result = f'<h{hdings}>'
+        hash_count = text.count(symbol)
+        markd = text.split(symbol * hash_count + ' ')
+        result = f'<h{hash_count}>'
         result += "".join(markd).strip()
-        result += f'</h{hdings}>\n'
+        result += f'</h{hash_count}>\n'
         return result
 
-    def unordered_list(self):
+    def html_lists(self, symbol):
+        """Handle html ul ol parsing.
+        """
+        # settings
+        tag_symnol = {"-": "ul", "*": "ol"}
+        # initialize
+        tag = tag_symnol[symbol]
+        cache_list = self.unrd_list if symbol == 'ul' else self.ord_list
+
         text = self.filecontent[self.position]
-        extracted = text.split('- ')
-        self.unrd_list.append("<li>" + ''.join(extracted)
-                                     + "</li>\n")
+        extracted = text.split(f'{symbol} ')
+        cache_list.append("<li>" + ''.join(extracted)
+                                 + "</li>\n")
 
         if self.position + 1 < len(self.filecontent) and\
-                self.filecontent[self.position + 1].startswith('-'):
+                self.filecontent[self.position + 1].startswith(f'{symbol}'):
             self.position += 1
-            self.unordered_list()
-        if self.unrd_list:
-            return f"<ul>\n{''.join(self.unrd_list)}</ul>\n"
-
-    def ordered_list(self):
-        text = self.filecontent[self.position]
-        extracted = text.split('* ')
-        self.ord_list.append("<li>" + ''.join(extracted) + "</li>\n")
-
-        if self.filecontent[self.position + 1].startswith('* '):
-            self.position += 1
-            self.ordered_list()
-        if self.ord_list:
-            return f"<ol>\n{''.join(self.ord_list)}</ol>\n"
+            self.html_lists(symbol)
+        if cache_list:
+            return f"<{tag}>\n{''.join(cache_list)}</{tag}>\n"
 
     def parser(self, text, position):
         self.position = position
         pointers = {
             "#": self.heading,
-            "-": self.unordered_list,
-            "*": self.ordered_list,
+            "-": self.html_lists,
+            "*": self.html_lists,
             "": "",
         }
-        splited = text.split(' ')[0]
-        if splited[0] in pointers.keys():
-            result, pos = pointers[splited[0]](), self.position
+        tag = text.split(' ')[0][0]
+        if tag in pointers.keys():
+            result, pos = pointers[tag](tag), self.position
             # clear cache
-            self.unrd_list.clear()
-            self.ord_list.clear()
+            self.clear_cache()
             return result, pos
         return None, self.position
 
